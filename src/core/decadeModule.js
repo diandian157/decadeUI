1888a50f3d522fd166e06e26847ab11f95675385
gcw_64AtKWmu <344902030@qq.com>
2026-06-28 19:37:24 +0800

    fix: add threedol to decadeLayout.css selectors + update 修改指南

--- src/core/decadeModule.js
+++ src/core/decadeModule.js
@@ -20,7 +20,7 @@ const STYLE_TO_SKIN = {
 	onlineUI: "online",
 	babysha: "baby",
 	codename: "codename",
-	threedol: "online",
+	threedol: "3dol",
 };
 
 /** @type {Object<string, number>} 样式到索引的映射 */
@@ -31,7 +31,7 @@ const STYLE_TO_INDEX = {
 	onlineUI: 4,
 	babysha: 5,
 	codename: 6,
-	threedol: 4,
+	threedol: 7,
 };
 
 /**
