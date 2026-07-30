0e5469aeab0f8f6aa190924f10a4ba17ee6a2b25
藤田ことね <x94106038@gmail.com>
2026-07-30 19:58:12 +0800

    移除小零食

--- src/core/decadeModule.js
+++ src/core/decadeModule.js
@@ -3,7 +3,6 @@
  */
 import { lib, game, ui, get, ai, _status } from "noname";
 import { createScriptElement, createLinkElement } from "./loader.js";
-import { checkVersionCompatibility } from "../utils/version.js";
 import { prefixMarkModule } from "../ui/prefixMark.js";
 
 /** @type {Array<string>} 排除的游戏模式 */
@@ -49,8 +48,6 @@ function getConfigValue(key, defaultValue) {
  * @returns {Object} 模块对象
  */
 export function initDecadeModule() {
-	checkVersionCompatibility();
-
 	if (!ui.css.layout) return {};
 	if (!ui.css.layout.href?.includes("long2")) {
 		ui.css.layout.href = `${lib.assetURL}layout/long2/layout.css`;
